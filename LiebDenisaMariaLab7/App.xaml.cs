4f9fbe462faee45e864bee77ab3b9501cacf3d70
LiebDenisa <denisalieb@gmail.com>
2024-11-25 21:07:40 +0200

    Lab 8 complete

--- LiebDenisaMariaLab7/App.xaml.cs
+++ LiebDenisaMariaLab7/App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace LiebDenisaMariaLab7
+﻿using System;
+using LiebDenisaMariaLab7.Data;
+using System.IO;
+namespace LiebDenisaMariaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
