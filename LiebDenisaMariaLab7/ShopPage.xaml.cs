5623a623eae372a83ea9db00c6616817b6417b29
LiebDenisa <denisalieb@gmail.com>
2024-12-22 19:34:46 +0200

    Lab10

---
+++ LiebDenisaMariaLab7/ShopPage.xaml.cs
@@ -0,0 +1,66 @@
+using LiebDenisaMariaLab7;
+using Microsoft.Maui.Devices.Sensors;
+using LiebDenisaMariaLab7.Models;
+using Plugin.LocalNotification;
+
+
+namespace LiebDenisaMariaLab7;
+
+public partial class ShopPage : ContentPage
+{
+	public ShopPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        await App.Database.SaveShopAsync(shop);
+        await Navigation.PopAsync();
+    }
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        var address = shop.Adress;
+        var locations = await Geocoding.GetLocationsAsync(address);
+
+        var options = new MapLaunchOptions
+        {
+            Name = "Magazinul meupreferat" };
+        var shoplocation = locations?.FirstOrDefault();
+        var myLocation = await Geolocation.GetLocationAsync();
+        /* var myLocation = new Location(46.7731796289, 23.6213886738);
+       //pentru Windows Machine */
+        var distance = myLocation.CalculateDistance(shoplocation, DistanceUnits.Kilometers);
+        if (distance < 5)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+        await Map.OpenAsync(shoplocation, options);
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var shop = (Shop)BindingContext;
+            bool confirm = await DisplayAlert("Confirm Delete",
+                                              $"Are you sure you want to delete the shop: {shop.ShopName}?",
+                                              "Yes", "No");
+
+            if (confirm)
+            {
+                await App.Database.DeleteShopAsync(shop); // Ensure you have a `DeleteShopAsync` method in your database class.
+                await Navigation.PopAsync();
+            }
+        }
+
+    }
+
+}
